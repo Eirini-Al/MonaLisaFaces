db1e23599f3eca306579be0ab671ef03d0d26a34
Eirini-Al <eiraloupi01@gmail.com>
2024-12-12 14:14:38 +0200

    -updated info ui (WIP) -added new paintings (Mona Vanna, Le Rire)

--- Assets/Scripts/Painting.cs
+++ Assets/Scripts/Painting.cs
@@ -9,6 +9,6 @@ public class Painting : ScriptableObject
     public string p_Name;
     public Material p_Material;
     public string p_Creator;
-    [TextArea(3,10)]
+    [TextArea(3,30)]
     public string p_Description;
 }
--- Assets/Scripts/PaintingManager.cs
+++ Assets/Scripts/PaintingManager.cs
@@ -1,24 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Vuforia;
+using UnityEngine.UI;
 using TMPro;
 using System;
 
 public class PaintingManager : MonoBehaviour
 {
-    [SerializeField] ImageTargetBehaviour m_ImageTarget;
+    [Header("Painting Elements")]
+    //[SerializeField] ImageTargetBehaviour m_ImageTarget;
     [SerializeField] GameObject m_PaintingPlane;
     [SerializeField] TextMeshProUGUI m_PaintingName;
     [SerializeField] TextMeshProUGUI m_PaintingCreator;
     [SerializeField] Painting[] paintings;
+    [Space]
+    [Header("Info Panel Elements")]
+    [SerializeField] GameObject infoPanel;
+    [SerializeField] TextMeshProUGUI infoText;
+    [SerializeField] Button openInfoButton;
+    [SerializeField] Button closeInfoButton;
     int currentIndex = 0;
     // Start is called before the first frame update
     void Start()
     {
+        CloseInfo();
         //m_Painting = paintings[currentIndex];
         SetPainting(paintings[currentIndex]);
+        openInfoButton.onClick.AddListener(ShowInfo);
+        closeInfoButton.onClick.AddListener(CloseInfo);
         
+    }
+
+    private void CloseInfo()
+    {
+        infoPanel.SetActive(false);
+        openInfoButton.gameObject.SetActive(true);
+        closeInfoButton.gameObject.SetActive(false);
+    }
+
+    private void ShowInfo()
+    {
+        infoPanel.SetActive(true);
+        openInfoButton.gameObject.SetActive(false);
+        closeInfoButton.gameObject.SetActive(true);
     }
 
     private void SetPainting(Painting m_Painting)
@@ -26,6 +50,7 @@ public class PaintingManager : MonoBehaviour
         m_PaintingPlane.GetComponent<MeshRenderer>().material = m_Painting.p_Material;
         m_PaintingName.text = m_Painting.p_Name;
         m_PaintingCreator.text = m_Painting.p_Creator;
+        infoText.text = m_Painting.p_Description;
     }
 
 
